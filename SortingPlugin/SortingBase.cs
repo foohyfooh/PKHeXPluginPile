d182f34a4f1fc98d8288ef8642376949b3bb39ec
Jonathan Herbert <foohyfooh@gmail.com>
2023-10-06 22:53:34 -0400

    Add Additional Sorting Options and Improve Settings Page
    - Add additional options to be used after sorting by Pokedéx.
    - Remove PluginSettings.settings and change to using non-autogenerated class so that descriptions can be shown in the SettingsForm.

--- SortingPlugin/SortingBase.cs
+++ SortingPlugin/SortingBase.cs
@@ -2,8 +2,8 @@ using PKHeX.Core;
 
 namespace PluginPile.SortingPlugin {
   class SortingBase {
-    
-    private static (int, int, int) ProduceComparable(Dictionary<Species, PositionForms>[] dexes, PKM pkm) {
+
+    private static (int, int, int) SortPokemon(Dictionary<Species, PositionForms>[] dexes, PKM pkm) {
       for (int i = 0; i < dexes.Length; i++) {
         Species species = (Species)pkm.Species;
         if (dexes[i].ContainsKey(species) && ((PluginSettings.Default.CheckForms && dexes[i][species].Forms.Contains(pkm.Form)) || !PluginSettings.Default.CheckForms)) {
@@ -15,8 +15,23 @@ namespace PluginPile.SortingPlugin {
 
     protected static Func<PKM, IComparable>[] GenerateSortingFunctions(params Dictionary<Species, PositionForms>[] dexes) {
       Func<PKM, IComparable>[] sortFunctions = new Func<PKM, IComparable>[] {
-        (PKM p) => ProduceComparable(dexes, p),
+        (PKM p) => SortPokemon(dexes, p),
       };
+      // TODO: Optimise Adding Options To Array
+      switch(PluginSettings.Default.AdditionalSortOptions) {
+        case PluginSettings.SortOptions.Level:
+          sortFunctions = sortFunctions.Append((PKM p) => p.CurrentLevel).ToArray();
+          break;
+        case PluginSettings.SortOptions.MetDate:
+          sortFunctions = sortFunctions.Append((PKM p) => p.MetDate!).ToArray();
+          break;
+        case PluginSettings.SortOptions.LevelThenMetDate:
+          sortFunctions = sortFunctions.Append((PKM p) => p.CurrentLevel).Append((PKM p) => p.MetDate!).ToArray();
+          break;
+        case PluginSettings.SortOptions.MetDateThenLevel:
+          sortFunctions = sortFunctions.Append((PKM p) => p.MetDate!).Append((PKM p) => p.CurrentLevel).ToArray();
+          break;
+      }
       return sortFunctions;
     }
 
