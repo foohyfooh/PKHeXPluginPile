fcfa72809e8aedaf9471a687e2548d045f59f7ae
Jonathan Herbert <foohyfooh@gmail.com>
2023-10-24 22:30:09 -0400

    Update Build Script To Bundle Unmaintained Plugins

--- build.ps1
+++ build.ps1
@@ -2,13 +2,15 @@ dotnet publish -c release
 if (Test-Path "bin\Build\") {
   Remove-Item -Path "bin\Build\" -Recurse
 }
-if (Test-Path "bin\PluginPile.dll") {
-  Remove-Item -Path "bin\PluginPile.dll"
-}
 if (Test-Path "bin\PKHeXPluginPile.zip") {
   Remove-Item -Path "bin\PKHeXPluginPile.zip"
 }
 New-Item -Path "bin\" -Name "Build" -ItemType "directory"
-Copy-Item -Path "bin\Release\net7.0-windows\publish\*.dll" -Destination "bin\Build" -Exclude "PKHeX.Core.dll","PluginPile.dll","PluginPile.Common.dll","PluginPile.Unmaintained.*.dll"
-Copy-Item -Path "bin\Release\net7.0-windows\publish\PluginPile.dll" -Destination "bin"
+New-Item -Path "bin\" -Name "Build\Individual" -ItemType "directory"
+Copy-Item -Path "bin\Release\net7.0-windows\publish\PluginPile.*.dll" -Destination "bin\Build\Individual" -Exclude "PluginPile.Common.dll","PluginPile.Unmaintained.*.dll"
+Copy-Item -Path "bin\Release\net7.0-windows\publish\PluginPile.dll" -Destination "bin\Build"
+New-Item -Path "bin\" -Name "Build\Unmaintained" -ItemType "directory"
+Copy-Item -Path "bin\Release\net7.0-windows\publish\PluginPile.Unmaintained.*.dll" -Destination "bin\Build\Unmaintained"
+Copy-Item -Path "bin\x64\Release\net7.0-windows\publish\PluginPile.Unmaintained.*.dll" -Destination "bin\Build\Unmaintained"
+Copy-Item -Path "bin\x64\Release\net7.0-windows\publish\*z3.dll" -Destination "bin\Build\Unmaintained"
 Compress-Archive -Path "bin\Build\*" -DestinationPath "bin\PKHeXPluginPile.zip"
