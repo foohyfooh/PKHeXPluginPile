d404b15a2c484acb8c786c1649d313a9107b25ec
Jonathan Herbert <foohyfooh@gmail.com>
2024-02-22 17:24:51 -0400

    Use TeamIndexes To Get Battle Teams

--- PluginPile.TeamViewer/BattleTeams.cs
+++ PluginPile.TeamViewer/BattleTeams.cs
@@ -5,19 +5,18 @@ namespace PluginPile.TeamViewer;
 public class BattleTeams : TeamsBase {
 
   public BattleTeams(SAV8SWSH sav) : base(Constants.SwSh.NumBattleTeams) {
-    byte[] teamsData = sav.Blocks.GetBlock(Constants.SwSh.BattleTeamsIndexes).Data;
+    sav.TeamIndexes.LoadBattleTeams();
     byte[] namesData = sav.Blocks.GetBlock(Constants.SwSh.BatleTeamsNames).Data;
-    ExtractSwShOrSV(sav, Constants.SwSh.NumBattleTeams, teamsData, namesData, Constants.SwSh.BattleTeamNameSize);
+    ExtractTeamFromIndexes(sav, Constants.SwSh.NumBattleTeams, sav.TeamIndexes, namesData, Constants.SwSh.BattleTeamNameSize);
   }
 
   public BattleTeams(SAV9SV sav) : base(Constants.SV.NumBattleTeams) {
-    byte[] teamsData = sav.Blocks.GetBlock(Constants.SV.BattleTeamsIndexes).Data;
+    sav.TeamIndexes.LoadBattleTeams();
     byte[] namesData = sav.Blocks.GetBlock(Constants.SV.BatleTeamsNames).Data;
-    ExtractSwShOrSV(sav, Constants.SV.NumBattleTeams, teamsData, namesData, Constants.SV.BattleTeamNameSize);
+    ExtractTeamFromIndexes(sav, Constants.SV.NumBattleTeams, sav.TeamIndexes, namesData, Constants.SV.BattleTeamNameSize);
   }
 
-  private void ExtractSwShOrSV(SaveFile sav, int numTeams, byte[] indexes, byte[] names, int nameSize) {
-    int indexSize = 0x0C; // 2 bytes (slot and box) for each member
+  private void ExtractTeamFromIndexes(SaveFile sav, int numTeams, ITeamIndexSet indexes, byte[] names, int nameSize) {
     for (int i = 0; i < numTeams; i++) {
       byte[] nameBytes = names
         .Skip(i * nameSize)
@@ -25,19 +24,7 @@ public class BattleTeams : TeamsBase {
         .ToArray();
       string name = StringUtils.GetUnicodeString(nameBytes);
       if (name.Length == 0) name = $"{Language.Team} {i + 1}";
-      byte[] teamIndexes = indexes
-        .Skip(i * indexSize)
-        .Take(indexSize)
-        .ToArray();
-      List<PKM> mons = [];
-      for (int j = 0; j < Constants.NumTeamMons; j++) {
-        byte slot = teamIndexes[j * 2    ];
-        byte box  = teamIndexes[j * 2 + 1];
-        if (slot != 0xFF && box != 0xFF) {
-          PKM mon = sav.GetBoxSlotAtIndex(box, slot);
-          mons.Add(mon);
-        }
-      }
+      List<PKM> mons = indexes.GetTeam(sav, i);
       Teams.Add(new BattleTeam(name, mons));
     }
   }
---
+++ PluginPile.TeamViewer/TeamIndexesExtensions.cs
@@ -0,0 +1,23 @@
+using PKHeX.Core;
+
+namespace PluginPile.TeamViewer;
+
+public static class TeamIndexesExtensions {
+
+  public static List<PKM> GetTeam(this ITeamIndexSet teamIndexes, SaveFile sav, int i) {
+     int[] slots = teamIndexes switch {
+      TeamIndexes8 indexes8 => indexes8.TeamSlots,
+      TeamIndexes9 indexes9 => indexes9.TeamSlots,
+      _ => throw new ArgumentException("Invalid Team Indexes")
+    };
+
+    List<PKM> team = [];
+    for(int j = i * 6, end = j + 6; j < end; j++) {
+      if (slots[j] != -1) {
+        team.Add(sav.GetBoxSlotAtIndex(slots[j]));
+      }
+    }
+    return team;
+  }
+
+}
