74d611fb5ad758a31daead869cbe0e19d3a92832
Eelen <37260264+ppllouf@users.noreply.github.com>
2024-03-11 21:54:39 +0800

    Update CHS Translations
    Co-Authored-By: Professor Dirty <103500840+wubinwww@users.noreply.github.com>

--- PluginPile.SpecialPkmnFinder/Language.cs
+++ PluginPile.SpecialPkmnFinder/Language.cs
@@ -6,6 +6,7 @@ internal static class Language {
   public static string Finders {
     get {
       return GameInfo.CurrentLanguage switch {
+        "zh" => "宝可梦搜寻器",
         "en" or _ => "Pokémon Finders"
       };
     }
@@ -23,6 +24,7 @@ internal static class Language {
   public static string WurmpleEvolutionFinder {
     get {
       return GameInfo.CurrentLanguage switch {
+        "zh" => "刺尾虫进化搜寻器",
         "en" or _ => "Wurmple Evolution Finder"
       };
     }
@@ -31,6 +33,7 @@ internal static class Language {
   public static string NoResults {
     get {
       return GameInfo.CurrentLanguage switch {
+        "zh" => "未找到结果",
         "en" or _ => "No Results Found"
       };
     }
